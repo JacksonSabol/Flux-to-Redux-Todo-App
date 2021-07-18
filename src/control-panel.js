906283b80fdbc4cc0b05d4f440954260d21994e2
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2021-07-17 19:11:20 -0700

    add a user preferences store to begin retrieving and eventually updating state values in the store with our dispatcher

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,4 +1,4 @@
-import { Dispatcher } from "./flux";
+import { Dispatcher, Store } from "./flux";
 // Create a new instance of the dispatcher
 const controlPanelDispatcher = new Dispatcher();
 
@@ -18,6 +18,42 @@ document.forms.fontSizeForm.fontSize.forEach(element => {
         controlPanelDispatcher.dispatch('TODO_FONT_SIZE_CHANGE');
     });
 });
+
+// Now we will define a new store type for holding user preferences, like font size and username
+class UserPrefsStore extends Store {
+    // Remember that we have to define getInitialState and _onDispatch for specific stores, to override the empty methods of the template store
+    getInitialState() {
+        // This method will just return the default values for the intial load
+        // We'll just use the hardcoded values from the index.html file
+        return {
+            username: 'Jim',
+            fontSize: 'small'
+        };
+    }
+
+    _onDispatch(action) {
+        // Since our actions aren't set up yet, we'll just log some info
+        console.log('Store received dispatch: ', action);
+        // Then we want to let our listeners know that something has been dispatched, so we emit the change
+        this._emitChange()
+    }
+
+    // Finally, we'll add something new that's not part of the base store: 
+    getUserPrefs() {
+        // Stores do not change the state, as state is immutable, but we can get a copy of the state
+        // So we'll just return the current state
+        return this._state;
+    }
+}
+
+// Now we initialize the store
+const usersPrefsStore = new UserPrefsStore(controlPanelDispatcher);
+
+// Test the store by adding a listener (similar to registering an action with the dispatcher)
+usersPrefsStore.addListener((state) => {
+    // the listener can just log the current state for now
+    console.info('The current state is: ', state);
+})
 
 // Registering the action with the dispatcher of logging the action name
 controlPanelDispatcher.register(action => {
